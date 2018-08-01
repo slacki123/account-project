e0cf7776b3d4a5bafe7cc5bc58ff497cdc94ef9f
slacki123 <sebastian.lacki@academytrainee.com>
2018-08-01 14:39:30 +0100

    update

--- Account-Application/src/project/Main.java
+++ Account-Application/src/project/Main.java
@@ -8,15 +8,6 @@ public class Main {
 
 	public static void main(String[] args) {
 
-//		Service service = new Service();
-//
-//		HashMap<Integer, Account> accountList = new HashMap<Integer, Account>();
-//
-//		Account acc1 = new Account("John", "Mills", 12345);
-//		accountList.put(1, acc1);
-//		JSONObject json = new JSONObject(accountList);
-//
-//		System.out.println(json);
 
 		Service service = new Service();
 
